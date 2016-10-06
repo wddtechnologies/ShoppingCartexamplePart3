5346a52bd82efdcc07694f48321134dd3ac38749
Louise <wddtechnologies@gmail.com>
2016-10-06 09:32:16 -0400

    Example of shopping cart for class
    This displays cart items, product items and set up with MVC - using Java
    Android and Java Jar with C++ ... remember this is example code

---
+++ ShoppingCart2016/app/src/main/java/com/example/teacher/shoppingcart2016/sc/model/Saleable.java
@@ -0,0 +1,12 @@
+package com.example.teacher.shoppingcart2016.sc.model;
+
+import java.math.BigDecimal;
+
+/**
+ * Implements this interface for any product which can be added to shopping cart
+ */
+public interface Saleable {
+    BigDecimal getPrice();
+
+    String getName();
+}
